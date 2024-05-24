2cbd3a1d2900fcb6d590239a8dd3a45071a48917
Clément Murzeau <clement@studiorelief.fr>
2024-05-24 15:55:15 +0200

    remove accordion script

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import { autoTabs } from '$utils/autotabs';
 import { darkModeContent } from '$utils/darkmode';
 import { loopArtists, loopBaseline } from '$utils/gsap';
 import { toggleSize } from '$utils/navbar';
-import { loadScript } from '$utils/scripts';
+/* import { loadScript } from '$utils/scripts'; */
 import { swiperLab, swiperProject, swiperSlide, swiperTestimonials } from '$utils/swiper';
 import { cmsPlaylist, svgCms } from '$utils/tricks';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  Promise.all([
+  /* Promise.all([
     loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-accordion@1/accordion.js'),
-  ]);
+  ]); */
 
   darkModeContent();
   toggleSize();
